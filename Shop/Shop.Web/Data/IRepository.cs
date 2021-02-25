c408bf7a5f8331ec8d5b8ba6f37e8c6a3a129e42
Miguel Velarde <mvelarde@astrum.com.mx>
2021-02-25 11:01:27 -0700

    add Interface Repository

---
+++ Shop/Shop.Web/Data/IRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace Shop.Web.Data
+{    
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Entities;
+
+    public interface IRepository
+    {
+        void AddProduct(Product p);
+        
+        Product GetProduct(int id);
+        
+        IEnumerable<Product> GetProducts();
+        
+        bool ProductExists(int id);
+        
+        void RemoveProduct(Product p);
+        
+        Task<bool> SaveAllAsync();
+        
+        void UpdateProduct(Product p);
+    }
+}
