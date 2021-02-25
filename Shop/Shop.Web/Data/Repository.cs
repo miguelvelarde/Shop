d9bacb983873e7b7abfe04a8224582cc2d5a0e37
Miguel Velarde <mvelarde@astrum.com.mx>
2021-02-25 10:57:49 -0700

    add repository class

---
+++ Shop/Shop.Web/Data/Repository.cs
@@ -0,0 +1,52 @@
+﻿namespace Shop.Web.Data
+{    
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Entities;
+
+    public class Repository
+    {
+        private readonly DataContext context;
+
+        public Repository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Product> GetProducts() 
+        {
+            return this.context.Products.OrderBy(p => p.Name);
+        }
+
+        public Product GetProduct(int id) 
+        {
+            return this.context.Products.Find(id);
+        }
+
+        public void AddProduct(Product p) 
+        {
+            this.context.Products.Add(p);
+        }
+
+        public void UpdateProduct(Product p)
+        {
+            this.context.Products.Update(p);
+        }
+
+        public void RemoveProduct(Product p)
+        {
+            this.context.Products.Remove(p);
+        }
+
+        public async Task<bool> SaveAllAsync() 
+        {
+            return await this.context.SaveChangesAsync() > 0;
+        }
+
+        public bool ProductExists(int id) 
+        {
+            return this.context.Products.Any(p => p.Id == id);
+        }
+    }
+}
